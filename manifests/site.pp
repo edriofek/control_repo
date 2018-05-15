18d45bdb13cf62d11e369ea5194f8e5a6a36835d
edriofek <39295197+edriofek@users.noreply.github.com>
2018-05-15 13:58:37 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,1 @@
-   node default  {
-      file  {'/root/README':
-         ensure => file,
-         content => 'This is a readme of ofek',
-         owner => 'root',
-         }
-         file {'/root/README':
-           owner => 'root',
-         }
-      }
+
